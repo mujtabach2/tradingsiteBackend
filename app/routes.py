ee1b41ff8a8d66229a3a28187028738a6ba17470
mujtabach2 <114252969+mujtabach2@users.noreply.github.com>
2024-01-24 03:25:19 -0500

    made pd to dict for jsonify

--- app/routes.py
+++ app/routes.py
@@ -5,5 +5,10 @@ bp = Blueprint('routes', __name__)
 
 @bp.route('/get_results', methods=['GET'])
 def send():
-    results = get_results()
-    return jsonify(results)
+     results = get_results()
+
+    # Convert DataFrame to a dictionary
+     results_dict = results.to_dict(orient='records')
+
+    # Return the dictionary as JSON
+     return jsonify(results_dict)
