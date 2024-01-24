637597ef3e23d173298b4efb930ea95834ddba9b
mujtabach2 <114252969+mujtabach2@users.noreply.github.com>
2024-01-24 01:10:16 -0500

    small change

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from app import app
 from flask import Blueprint, jsonify
 from functions.stockRl import get_results
+
 bp = Blueprint('routes', __name__)
 
 @bp.route('/get_results', methods=['GET'])
