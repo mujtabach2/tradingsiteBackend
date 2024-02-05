ad711ceb7853b3885b0b71283ac27aed81cef231
mujtabach2 <114252969+mujtabach2@users.noreply.github.com>
2024-02-05 00:06:12 -0500

    added cors

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,18 @@
-from flask import Blueprint, jsonify
+from flask import Flask, Blueprint, jsonify
+from flask_cors import CORS
 from functions.stockRl import get_results
 
 bp = Blueprint('routes', __name__)
 
+# Enable CORS for all routes on the blueprint
+CORS(bp)
+
 @bp.route('/get_results', methods=['GET'])
 def send():
-     results = get_results()
+    results = get_results()
 
     # Convert DataFrame to a dictionary
-     results_dict = results.to_dict(orient='records')
+    results_dict = results.to_dict(orient='records')
 
     # Return the dictionary as JSON
-     return jsonify(results_dict)
+    return jsonify(results_dict)
